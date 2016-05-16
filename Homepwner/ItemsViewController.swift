ecec53044d78a88d7ed4c3199ceb2ee55fa4232a
derekgilwa <dgliwa@thoughtworks.com>
2016-05-15 21:51:53 -0500

    Use reusable cells and make table view not overlap with status bar.

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -12,12 +12,22 @@ class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        let insets = UIEdgeInsets.init(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .Value1, reuseIdentifier:  "UITableViewCell")
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
         
         let item = itemStore.allItems[indexPath.row]
         cell.textLabel?.text = item.name
